308eb74f3340a15db0c1b6ce784430ca6b61cb6f
bodduvamshi <vamshi.boddu2001@gmail.com>
2024-06-29 13:40:50 +0530

    Subnet creation

--- VPC.tf
+++ VPC.tf
@@ -9,3 +9,40 @@ resource "aws_vpc" "test-vpc" {
   }
 }
 
+#Subnet web creation
+
+resource "aws_subnet" "test-web-sub" {
+  vpc_id     = aws_vpc.test-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "test-web-subnet"
+  }
+}
+
+#Subnet app creation
+
+resource "aws_subnet" "test-api-sub" {
+  vpc_id     = aws_vpc.test-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "test-api-subnet"
+  }
+}
+
+#Subnet db creation
+
+resource "aws_subnet" "test-db-sub" {
+  vpc_id     = aws_vpc.test-vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "test-db-subnet"
+  }
+}
+
+
+
