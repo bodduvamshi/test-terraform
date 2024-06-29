5f577b0f0f19c243a8de2a0121bb86310fff142d
bodduvamshi <vamshi.boddu2001@gmail.com>
2024-06-29 13:44:16 +0530

    internet gateway creation

--- VPC.tf
+++ VPC.tf
@@ -44,5 +44,15 @@ resource "aws_subnet" "test-db-sub" {
   }
 }
 
+#internet gateway creation
+
+resource "aws_internet_gateway" "test-ig" {
+  vpc_id = aws_vpc.test-vpc.id
+
+  tags = {
+    Name = "test-internet-gateway"
+  }
+}
+
 
 
