2e172170db4824648f05d7b369a38eac89133ca9
bodduvamshi <vamshi.boddu2001@gmail.com>
2024-06-30 10:46:18 +0530

    updated sercurity group db

--- VPC.tf
+++ VPC.tf
@@ -295,7 +295,7 @@ resource "aws_security_group" "test-db-sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "test-db-sg-ingress-ssh" {
   security_group_id = aws_security_group.test-db-sg.id
-  cidr_ipv4         = 0.0.0.0/0
+  cidr_ipv4         = 10.0.0.0/16
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
